675bfdd2aad16be0392dcdbe0f0bd2eec5b3b332
jmontalvo94 <50923223+jmontalvo94@users.noreply.github.com>
2020-02-04 23:22:28 +0100

    Update Exercises
    Lecture 1 exercises

--- Exercises/Lecture1/FourTankSystem.m
+++ Exercises/Lecture1/FourTankSystem.m
@@ -1,68 +1,3 @@
-%% Model
-
-function xdot = FourTankSystem(t,x,u,p)
-% FOURTANKSYSTEM Model dx/dt = f(t,x,u,p) for 4-tank System
-%
-% This function implements a differential equation model for the
-% 4-tank system.
-%
-% Syntax: xdot = FourTankSystem(t,x,u,p)
-
-% Unpack states, MVs, and parameters
-m = x; % Mass of liquid in each tank [g]
-F = u; % Flow rates in pumps [cm3/s]
-a = p(1:4,1); % Pipe cross sectional areas [cm2]
-A = p(5:8,1); % Tank cross sectional areas [cm2]
-gamma = p(9:10,1); % Valve positions [-]
-g = p(11,1); % Acceleration of gravity [cm/s2]
-rho = p(12,1); % Density of water [g/cm3]
-
-% Inflows
-qin = zeros(4,1);
-qin(1,1) = gamma(1)*F(1); % Inflow from valve 1 to tank 1 [cm3/s]
-qin(2,1) = gamma(2)*F(2); % Inflow from valve 2 to tank 2 [cm3/s]
-qin(3,1) = (1-gamma(2))*F(2); % Inflow from valve 2 to tank 3 [cm3/s]
-qin(4,1) = (1-gamma(1))*F(1); % Inflow from valve 1 to tank 4 [cm3/s]
-
-% Outflows
-h = m./(rho*A); % Liquid level in each tank [cm]
-qout = a.*sqrt(2*g*h); % Outflow from each tank [cm3/s]
-
-% Differential equations
-xdot = zeros(4,1);
-xdot(1,1) = rho*(qin(1,1)+qout(3,1)-qout(1,1)); % Mass balance Tank 1
-xdot(2,1) = rho*(qin(2,1)+qout(4,1)-qout(2,1)); % Mass balance Tank 2
-xdot(3,1) = rho*(qin(3,1)-qout(3,1)); % Mass balance Tank 3
-xdot(4,1) = rho*(qin(4,1)-qout(4,1)); % Mass balance Tank 4
-
-function y = FourTankSystemSensor(x,p)
-% FOURTANKSYSTEMSENSOR Level for each tank in the four tank system
-%
-% Syntax: y = FourTankSystemSensor(x,p)
-
-% Extract states and parameters
-m = x;
-A = p(5:8,1);
-rho = p(12,1);
-
-% Compute level in each tank
-rhoA = rho*A;
-y = m./rhoA;
-
-function z = FourTankSystemOutput(x,p)
-% FOURTANKSYSTEMOUTPUT Level for the lower tanks in the four tank system
-%
-% Syntax: z = FourTankSystemOutput(x,p)
-
-% Extract states and parameters
-m = x(1:2,1);
-A = p(5:6,1);
-rho = p(12,1);
-
-% Compute level in each tank
-rhoA = rho*A;
-z = m./rhoA;
-
 %% Driver
 
 % --------------------------------------------------------------
@@ -102,7 +37,7 @@ u = [F1; F2];
 % --------------------------------------------------------------
 
 % Solve the system of differential equations
-[T,X] = ode45(@FourTankSystem,[t0 tf],x0,[],u,p);
+[T,X] = ode45(@FourTankSystemModel,[t0 tf],x0,[],u,p);
 
 % --------------------------------------------------------------
 % Additional variables for plotting
@@ -130,3 +65,71 @@ end
 % --------------------------------------------------------------
 plot(H)
 plot(Qout)
+
+%% Model
+
+function xdot = FourTankSystemModel(t,x,u,p)
+% FOURTANKSYSTEM Model dx/dt = f(t,x,u,p) for 4-tank System
+%
+% This function implements a differential equation model for the
+% 4-tank system.
+%
+% Syntax: xdot = FourTankSystem(t,x,u,p)
+
+% Unpack states, MVs, and parameters
+m = x; % Mass of liquid in each tank [g]
+F = u; % Flow rates in pumps [cm3/s]
+a = p(1:4,1); % Pipe cross sectional areas [cm2]
+A = p(5:8,1); % Tank cross sectional areas [cm2]
+gamma = p(9:10,1); % Valve positions [-]
+g = p(11,1); % Acceleration of gravity [cm/s2]
+rho = p(12,1); % Density of water [g/cm3]
+
+% Inflows
+qin = zeros(4,1);
+qin(1,1) = gamma(1)*F(1); % Inflow from valve 1 to tank 1 [cm3/s]
+qin(2,1) = gamma(2)*F(2); % Inflow from valve 2 to tank 2 [cm3/s]
+qin(3,1) = (1-gamma(2))*F(2); % Inflow from valve 2 to tank 3 [cm3/s]
+qin(4,1) = (1-gamma(1))*F(1); % Inflow from valve 1 to tank 4 [cm3/s]
+
+% Outflows
+h = m./(rho*A); % Liquid level in each tank [cm]
+qout = a.*sqrt(2*g*h); % Outflow from each tank [cm3/s]
+
+% Differential equations
+xdot = zeros(4,1);
+xdot(1,1) = rho*(qin(1,1)+qout(3,1)-qout(1,1)); % Mass balance Tank 1
+xdot(2,1) = rho*(qin(2,1)+qout(4,1)-qout(2,1)); % Mass balance Tank 2
+xdot(3,1) = rho*(qin(3,1)-qout(3,1)); % Mass balance Tank 3
+xdot(4,1) = rho*(qin(4,1)-qout(4,1)); % Mass balance Tank 4
+end
+
+function y = FourTankSystemSensor(x,p)
+% FOURTANKSYSTEMSENSOR Level for each tank in the four tank system
+%
+% Syntax: y = FourTankSystemSensor(x,p)
+
+% Extract states and parameters
+m = x;
+A = p(5:8,1);
+rho = p(12,1);
+
+% Compute level in each tank
+rhoA = rho*A;
+y = m./rhoA;
+end
+
+function z = FourTankSystemOutput(x,p)
+% FOURTANKSYSTEMOUTPUT Level for the lower tanks in the four tank system
+%
+% Syntax: z = FourTankSystemOutput(x,p)
+
+% Extract states and parameters
+m = x(1:2,1);
+A = p(5:6,1);
+rho = p(12,1);
+
+% Compute level in each tank
+rhoA = rho*A;
+z = m./rhoA;
+end
--- Exercises/Lecture1/VanDerPol.m
+++ Exercises/Lecture1/VanDerPol.m
@@ -1,12 +1,20 @@
+%% Driver
+
+mu = 10;
+x0 = [2.0; 0.0];
+options = odeset('Jacobian',@JacVanDerPol,'RelTol',1.0e-6,'AbsTol',1.0e-6);
+[T,X]=ode15s(@VanDerPolModel,[0 5*mu],x0,options,mu);
+
 %% Model
 
-function xdot = VanDerPol(t,x,mu)
+function xdot = VanDerPolModel(t,x,mu)
 % VANDERPOL Implementation of the Van der Pol model
 %
 % Syntax: xdot = VanDerPol(t,x,mu)
 xdot=zeros(2,1);
 xdot(1) = x(2);
 xdot(2) = mu*(1-x(1)*x(1))*x(2)-x(1);
+end
 
 function Jac = JacVanDerPol(t,x,mu)
 % JACVANDERPOL Jacobian for the Van der Pol Equation
@@ -17,11 +25,4 @@ Jac = zeros(2,2);
 Jac(2,1) = -2*mu*x(1)*x(2)-1.0;
 Jac(1,2) = 1.0;
 Jac(2,2) = mu*(1-x(1)*x(1));
-
-
-%% Driver
-
-mu = 10;
-x0 = [2.0; 0.0];
-options = odeset('Jacobian',@JacVanDerPol,'RelTol',1.0e-6,'AbsTol',1.0e-6);
-[T,X]=ode15s(@VanDerPol,[0 5*mu],x0,options,mu);
+end

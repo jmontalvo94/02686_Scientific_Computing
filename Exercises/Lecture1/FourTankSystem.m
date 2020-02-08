27a9bfca375fa02d7595b9160129d588e2971784
jmontalvo94 <50923223+jmontalvo94@users.noreply.github.com>
2020-02-08 17:36:43 +0100

    Update FourTankSystem.m

--- Exercises/Lecture1/FourTankSystem.m
+++ Exercises/Lecture1/FourTankSystem.m
@@ -7,12 +7,15 @@ a1 = 1.2272; %[cm2] Area of outlet pipe 1
 a2 = 1.2272; %[cm2] Area of outlet pipe 2
 a3 = 1.2272; %[cm2] Area of outlet pipe 3
 a4 = 1.2272; %[cm2] Area of outlet pipe 4
+
 A1 = 380.1327; %[cm2] Cross sectional area of tank 1
 A2 = 380.1327; %[cm2] Cross sectional area of tank 2
 A3 = 380.1327; %[cm2] Cross sectional area of tank 3
 A4 = 380.1327; %[cm2] Cross sectional area of tank 4
+
 gamma1 = 0.45; % Flow distribution constant. Valve 1
 gamma2 = 0.40; % Flow distribution constant. Valve 2
+
 g = 981; %[cm/s2] The acceleration of gravity
 rho = 1.00; %[g/cm3] Density of water
 
@@ -21,23 +24,79 @@ p = [a1; a2; a3; a4; A1; A2; A3; A4; gamma1; gamma2; g; rho];
 % --------------------------------------------------------------
 % Simulation scenario
 % --------------------------------------------------------------
+
+% Normal scenario
+% t0 = 0.0; % [s] Initial time
+% tf = 20*60; % [s] Final time
+% 
+% m10 = 0.0; % [g] Liquid mass in tank 1 at time t0
+% m20 = 0.0; % [g] Liquid mass in tank 2 at time t0
+% m30 = 0.0; % [g] Liquid mass in tank 3 at time t0
+% m40 = 0.0; % [g] Liquid mass in tank 4 at time t0
+% 
+% F1 = 300; % [cm3/s] Flow rate from pump 1
+% F2 = 300; % [cm3/s] Flow rate from pump 2
+% 
+% x0 = [m10; m20; m30; m40];
+% u = [F1; F2];
+
+% Discrete scenario with noise
 t0 = 0.0; % [s] Initial time
 tf = 20*60; % [s] Final time
-m10 = 0.0; % [g] Liquid mass in tank 1 at time t0
-m20 = 0.0; % [g] Liquid mass in tank 2 at time t0
-m30 = 0.0; % [g] Liquid mass in tank 3 at time t0
-m40 = 0.0; % [g] Liquid mass in tank 4 at time t0
+Ts = 10; % [s] Sample Time
+t = [t0:Ts:tf]'; % [s] Sample instants
+N = length(t);
+
+m10 = 0; % [g] Liquid mass in tank 1 at time t0
+m20 = 0; % [g] Liquid mass in tank 2 at time t0
+m30 = 0; % [g] Liquid mass in tank 3 at time t0
+m40 = 0; % [g] Liquid mass in tank 4 at time t0
+
 F1 = 300; % [cm3/s] Flow rate from pump 1
 F2 = 300; % [cm3/s] Flow rate from pump 2
+
 x0 = [m10; m20; m30; m40];
-u = [F1; F2];
+u = [repmat(F1,1,N); repmat(F2,1,N)];
+
+% Process Noise
+Q = [20^2 0;0 40^2];
+Lq = chol(Q,'lower');
+w = Lq*randn(2,N);
+
+% Measurement Noise
+R = eye(4);
+Lr = chol(R,'lower');
+v = Lr*randn(4,N);
 
 % --------------------------------------------------------------
 % Compute the solution / Simulate
 % --------------------------------------------------------------
 
-% Solve the system of differential equations
-[T,X] = ode45(@FourTankSystemModel,[t0 tf],x0,[],u,p);
+% Solve the system of differential equations [normal]
+%[T,X] = ode45(@FourTankSystemModel,[t0 tf],x0,[],u,p);
+
+% Solve the system of differential equations [discrete with noise]
+nx = 4; nu = 2; ny = 4; nz = 2;
+x = zeros(nx,N);
+y = zeros(ny,N);
+z = zeros(nz,N);
+X = zeros(0,nx);
+T = zeros(0,1);
+x(:,1) = x0;
+
+for k = 1:N-1
+    y(:,k) = FourTankSystemSensor(x(:,k),p)+v(:,k); % Sensor function
+    z(:,k) = FourTankSystemOutput(x(:,k),p); % Output function
+    [Tk,Xk] = ode45(@FourTankSystemModel,[t(k) t(k+1)],x(:,k),[],...
+        u(:,k)+w(:,k),p);
+    x(:,k+1) = Xk(end,:)';
+    T = [T; Tk];
+    X = [X; Xk];
+end
+
+k = N;
+y(:,k) = FourTankSystemSensor(x(:,k),p)+v(:,k); % Sensor function
+z(:,k) = FourTankSystemOutput(x(:,k),p); % Output function
 
 % --------------------------------------------------------------
 % Additional variables for plotting
@@ -60,11 +119,14 @@ for i=1:nT
     Qout(i,:) = a.*sqrt(2*g*H(i,:));
 end
 
+%% Data Visualization
 % --------------------------------------------------------------
 % Plotting
 % --------------------------------------------------------------
-plot(H)
-plot(Qout)
+
+Figure2by2(T,X/1000,nX,'Masses','m[kg]') %Divided over 1000 to transform to kg
+Figure2by2(T,H,nX,'Height','h[cm]')
+Figure2by2(T,Qout,nX,'Outflow Rate','q[cm^3/s]')
 
 %% Model
 
@@ -104,6 +166,8 @@ xdot(3,1) = rho*(qin(3,1)-qout(3,1)); % Mass balance Tank 3
 xdot(4,1) = rho*(qin(4,1)-qout(4,1)); % Mass balance Tank 4
 end
 
+
+
 function y = FourTankSystemSensor(x,p)
 % FOURTANKSYSTEMSENSOR Level for each tank in the four tank system
 %
@@ -132,4 +196,17 @@ rho = p(12,1);
 % Compute level in each tank
 rhoA = rho*A;
 z = m./rhoA;
+end
+
+function PlottedFigure = Figure2by2(a,b,n,name,yname)
+PlottedFigure = figure;
+t=tiledlayout(2,2);
+title(t,name)
+for i=1:n
+    nexttile
+    plot(a(:,1)/60,b(:,i)) % Divided over 60 to use minutes instead of second
+    title("Tank "+i)
+    xlabel('time[min]')
+    ylabel(yname)
+end
 end
